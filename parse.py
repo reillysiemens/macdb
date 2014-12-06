d666ff9aadbbde8a6a4164b9607fc25440e1a1d7
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-12-06 14:37:37 -0800

    Parse companies, convert to list of dictionaries.

--- parse.py
+++ parse.py
@@ -1,11 +1,17 @@
 import re
+import hashlib
 import requests
 
 location = "http://www.ieee.org/netstorage/standards/oui.txt"
 number_name = re.compile(" *(\w{6}) *\(.*\)[^\w]+(.*)$")
+oui_hash = hashlib.sha1()
+companies = []
 
 # Get the listing from the source location.
 req = requests.get(location)
+
+# Update our hash object with the value from our request string.
+oui_hash.update(bytes(req.text, "utf-8"))
 
 # Ignore the first 127 characters of junk data.
 req_string = req.text[127:]
@@ -13,7 +19,11 @@ req_string = req.text[127:]
 # Break the request string into a list of entries.
 entries = req_string.split('\r\n\r\n')
 
-for entry in entries:
-    matches = number_name.search(entry.split('\r\n')[1])
+# Remove junk entry at the end.
+del entries[-1]
 
-    print("{}: {}".format(matches.group(2), matches.group(1)))
+for entry in entries:
+    lines = entry.split('\r\n')
+    matches = number_name.search(lines[1])
+    company = {'name': matches.group(2), 'oui': matches.group(1)}
+    companies.append(company)
