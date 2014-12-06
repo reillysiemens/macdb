631919982f1567ab2ae99fe3d6edfdc4320e67d6
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-12-06 01:11:45 -0800

    Match name and number of company with a regular expression.

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
+import re
 import requests
 
 location = "http://www.ieee.org/netstorage/standards/oui.txt"
+number_name = re.compile(" *(\w{6}) *\(.*\)[^\w]+(.*)$")
 
 # Get the listing from the source location.
 req = requests.get(location)
@@ -10,3 +12,8 @@ req_string = req.text[127:]
 
 # Break the request string into a list of entries.
 entries = req_string.split('\r\n\r\n')
+
+for entry in entries:
+    matches = number_name.search(entry.split('\r\n')[1])
+
+    print("{}: {}".format(matches.group(2), matches.group(1)))
